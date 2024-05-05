7e51e9d980b5b4343ecebd35464ba682c030e156
MohamedEssam2127 <msam01619@gmail.com>
2024-05-05 18:14:00 +0300

    List one category

---
+++ app/home/products/index.js
@@ -0,0 +1,33 @@
+import { View, Text,TouchableOpacity, } from 'react-native'
+import React from 'react'
+import Items from '../../../components/Items'
+import Categorys from '../../../components/Categorys'
+import { Stack } from 'expo-router'
+import { MaterialIcons } from '@expo/vector-icons';
+export default function Products() {
+  return (
+    <View>
+       <Stack.Screen
+        options={{
+          // https://reactnavigation.org/docs/headers#setting-the-header-title
+          title: "Category",
+          // https://reactnavigation.org/docs/headers#adjusting-header-styles
+          headerStyle: { backgroundColor: "#ffb01d" },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize:30,
+          },
+          // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
+          // headerTitle: props => <LogoTitle {...props} />,
+          headerLeft: (props) => (
+            <TouchableOpacity  >
+         <MaterialIcons name="arrow-back-ios-new" size={24} color="white" />
+        </TouchableOpacity>
+          ),
+        }}
+      />
+     <Categorys/>
+    </View>
+  )
+}
---
+++ components/Categorys.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import Items from "./Items";
+// import pizza from "../assets/images/Pizza-icon.png";
+// import fries from "../assets/images/images.png";
+export default function Categorys() {
+  const [list, setList] = useState([
+    {
+      title: "pizza Margarita",
+      price: 100,
+      type: "Go Back to where it all began with the classic cheese and tomato base",
+      //image: pizza,
+      id:1,
+    },
+    {
+      title: "Potato Wedges",
+      price: 50,
+      type: "Crispy, oven baked potato wedges",
+      // image: fries,
+      id:2,
+    },
+    {
+      title: "pizza Pepperoni",
+      price: 140,
+      type: "One of our all time specialties. A meaty feast of Pepperoni, Mushroom, Black Olives, mozzarella cheese and tomato sauce",
+     // image: pizza,
+      id:3,
+    },
+  ]);
+  return (
+    <>
+      <FlatList
+        data={list}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.container}>
+              <Items item={item} />
+            </View>
+          );
+        }}
+      />
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
+});
