229466eacf73bc8a1e869a8852f323a98849832e
MohamedEssam2127 <msam01619@gmail.com>
2024-05-05 23:24:04 +0300

    data

--- app/home/products/index.js
+++ app/home/products/index.js
@@ -7,22 +7,17 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome } from "@expo/vector-icons";
 export default function Products() {
   const { name } = useGlobalSearchParams();
-  console.log("xxxxxxx",name);
   return (
     <View>
        <Stack.Screen
         options={{
-          // https://reactnavigation.org/docs/headers#setting-the-header-title
           title: name,
-          // https://reactnavigation.org/docs/headers#adjusting-header-styles
           headerStyle: { backgroundColor: "#ffb01d" },
           headerTintColor: "#fff",
           headerTitleStyle: {
             fontWeight: "bold",
             fontSize:30,
           },
-          // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
-          // headerTitle: props => <LogoTitle {...props} />,
           headerLeft: (props) => (
             <TouchableOpacity onPress={()=>router.back()} >
         <FontAwesome name="chevron-left" size={30} color="white"style={{paddingRight:10}} />
