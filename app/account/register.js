70122f97372aee8f25ecd1384b965b08da4368bb
MostafaAssar <137010623+MostafaAssar@users.noreply.github.com>
2024-05-09 19:50:32 +0300

    delete app directory to solve the problem

--- app/account/register.js
+++
@@ -1,7 +0,0 @@
-import Register from "../../screens/Register"
-
-export default function register () {
-    return(
-        <Register/>
-    )
-}
--- app/home/profile/index.js
+++
@@ -1,6 +0,0 @@
-import Profile from '../../../components/Profile'
-export default function Profile1() {
-    return (
-<Profile/>
-    )
-}
