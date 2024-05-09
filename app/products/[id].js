ff29b844ba8e8e08184aeebd0db50c2668d908ff
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2024-05-09 22:12:35 +0300

    Update item with firebase

--- app/products/[id].js
+++ app/products/[id].js
@@ -1,64 +1,104 @@
-import { View, Text,TouchableOpacity, } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from "react";
 import { Stack, useGlobalSearchParams } from 'expo-router'
 import { MaterialIcons } from '@expo/vector-icons';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { db } from '../../firebase/Config';
-import Category from '../../components/Category';
+import AdminItem from '../../components/Admin/AdminItem'
 
 import Item from '../../components/Item'
 import { doc, getDoc } from 'firebase/firestore';
 export default function Products() {
-  let x ; 
-  const [item,setItem]= useState({});
+  let x;
+  const [item, setItem] = useState({});
+  const [flag, setFlag] = useState(false);
   const { fid } = useGlobalSearchParams();
-  const getUser = async ()=>{
- const uid = JSON.parse( await AsyncStorage.getItem("@user")).uid;
-x=uid;
-fetItem();
-} 
-
-const fetItem=async()=>{
-  console.log("ffff",docRef);
-  const docRef = doc(db, "users", x);
- 
-  try {
-    const doc = await getDoc(docRef);
-    const data = doc.data();
-    setItem(data);
-    console.log("done")
-    console.log(item.phone)
-  } catch (e) {
-    console.log("Error getting cached document:", e);
+  const getUser = async () => {
+    const uid = JSON.parse(await AsyncStorage.getItem("@user")).uid;
+    x = uid;
+    fetItem();
   }
-}
-useEffect(()=>{
-getUser();
-},[])
-if(item.isAdmin){
 
-}
+  const fetItem = async () => {
+    const docRef = doc(db, "users", x);
+
+    try {
+      const doc = await getDoc(docRef);
+      const data = doc.data();
+      setItem(data);
+      setFlag(true);
+    } catch (e) {
+      console.log("Error getting cached document:", e);
+    }
+  }
+  useEffect(() => {
+    getUser();
+  }, [])
   return (
-    <View style= {{flex:1}}>
-       <Stack.Screen
-        options={{
-          title: "item 1",
-          headerShown: false ,
-          headerStyle: { backgroundColor: "#ffb01d" },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-            fontSize:30,
-          },
-          headerLeft: (props) => (
-            <TouchableOpacity  >
-         <MaterialIcons name="arrow-back-ios-new" size={24} color="white" />
-        </TouchableOpacity>
-          ),
-        }}
-      />
-      {item.isAdmin ? <Category />: <Item id={fid}/> }
-     
-    </View>
+    <>
+      {!flag ? (
+        <View style={[styles.container, styles.loadingContainer]}>
+          <Stack.Screen
+            options={{
+              title: "item 1",
+              headerShown: false,
+              headerStyle: { backgroundColor: "#ffb01d" },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+                fontSize: 30,
+              },
+              headerLeft: (props) => (
+                <TouchableOpacity  >
+                  <MaterialIcons name="arrow-back-ios-new" size={24} color="white" />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+          <ActivityIndicator size="large" color="#ffb01d" />
+        </View>
+      ) : (
+        <View style={{ flex: 1 }}>
+          <Stack.Screen
+            options={{
+              title: "item 1",
+              headerShown: false,
+              headerStyle: { backgroundColor: "#ffb01d" },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+                fontSize: 30,
+              },
+              headerLeft: (props) => (
+                <TouchableOpacity  >
+                  <MaterialIcons name="arrow-back-ios-new" size={24} color="white" />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+          {item.isAdmin ? <AdminItem id={fid} /> : <Item id={fid} />}
+
+        </View>
+      )}
+
+    </>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ActivityIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginTop: '40%',
+    size: 100,
+  }
+})
